f279fbee73a89af4df1b8036aa60f797e2f1ef54
LZaw <28070263+LZaw@users.noreply.github.com>
2018-03-08 15:58:39 +0100

    Update obj_writer.h

--- examples/obj_sticher/obj_writer.h
+++ examples/obj_sticher/obj_writer.h
@@ -1,7 +1,7 @@
 #ifndef __OBJ_WRITER_H__
 #define __OBJ_WRITER_H__
 
-#include "tiny_obj_loader.h"
+#include "../../tiny_obj_loader.h"
 
 extern bool WriteObj(const std::string& filename, const tinyobj::attrib_t& attributes, const std::vector<tinyobj::shape_t>& shapes, const std::vector<tinyobj::material_t>& materials, bool coordTransform = false);
 
