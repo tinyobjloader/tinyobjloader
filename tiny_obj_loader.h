5dd2955e51778060eaabf418d7cc1d02066f4c9f
Trider12 <47952308+Trider12@users.noreply.github.com>
2022-10-06 14:25:55 +0600

    Add alternative aliases for displacement maps during mtl parsing (#343)

--- tiny_obj_loader.h
+++ tiny_obj_loader.h
@@ -2203,15 +2203,8 @@ void LoadMtl(std::map<std::string, int> *material_map,
     }
 
     // bump texture
-    if ((0 == strncmp(token, "map_bump", 8)) && IS_SPACE(token[8])) {
-      token += 9;
-      ParseTextureNameAndOption(&(material.bump_texname),
-                                &(material.bump_texopt), token);
-      continue;
-    }
-
-    // bump texture
-    if ((0 == strncmp(token, "map_Bump", 8)) && IS_SPACE(token[8])) {
+    if ((0 == strncmp(token, "map_bump", 8)) && IS_SPACE(token[8]) || 
+        (0 == strncmp(token, "map_Bump", 8)) && IS_SPACE(token[8])) {
       token += 9;
       ParseTextureNameAndOption(&(material.bump_texname),
                                 &(material.bump_texopt), token);
@@ -2232,6 +2225,15 @@ void LoadMtl(std::map<std::string, int> *material_map,
       material.alpha_texname = token;
       ParseTextureNameAndOption(&(material.alpha_texname),
                                 &(material.alpha_texopt), token);
+      continue;
+    }
+
+    // displacement texture
+    if ((0 == strncmp(token, "map_disp", 8)) && IS_SPACE(token[8]) ||
+        (0 == strncmp(token, "map_Disp", 8)) && IS_SPACE(token[8])) {
+      token += 9;
+      ParseTextureNameAndOption(&(material.displacement_texname),
+                                &(material.displacement_texopt), token);
       continue;
     }
 
