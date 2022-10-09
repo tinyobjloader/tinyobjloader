3ac7d9e5dc1f05d9bfc8d02b730acde529b865fc
Syoyo Fujita <syoyo@lighttransport.com>
2022-10-09 21:36:35 +0900

    Bump version to v2.0.0rc10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from pybind11.setup_helpers import Pybind11Extension#, build_ext
 from setuptools import setup
 
-__version__ = "2.0.0rc9"
+__version__ = "2.0.0rc10"
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
