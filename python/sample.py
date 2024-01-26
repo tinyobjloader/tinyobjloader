d64867c93b73e595854d8e754fd9de2270bd0d6e
Syoyo Fujita <syoyo@lighttransport.com>
2024-01-27 06:57:18 +0900

    Fix parsing [w] component in `v` line. Improve python binding.   - Expose more attrib to Python.

--- python/sample.py
+++ python/sample.py
@@ -13,6 +13,9 @@ except:
 
 filename = "../models/cornell_box.obj"
 
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
 
 reader = tinyobjloader.ObjReader()
 
@@ -35,8 +38,11 @@ if reader.Warning():
 
 attrib = reader.GetAttrib()
 print("len(attrib.vertices) = ", len(attrib.vertices))
+print("len(attrib.vertex_weights) = ", len(attrib.vertex_weights))
 print("len(attrib.normals) = ", len(attrib.normals))
 print("len(attrib.texcoords) = ", len(attrib.texcoords))
+print("len(attrib.colors) = ", len(attrib.colors))
+print("len(attrib.skin_weights) = ", len(attrib.skin_weights))
 
 # vertex data must be `xyzxyzxyz...`
 assert len(attrib.vertices) % 3 == 0
@@ -46,6 +52,9 @@ assert len(attrib.normals) % 3 == 0
 
 # texcoords data must be `uvuvuv...`
 assert len(attrib.texcoords) % 2 == 0
+
+# colors data must be `rgbrgbrgb...`
+assert len(attrib.texcoords) % 3 == 0
 
 # Performance note
 # (direct?) array access through member variable is quite slow.
@@ -63,16 +72,34 @@ assert len(attrib.texcoords) % 2 == 0
 for i, v in enumerate(attrib.vertices):
     print("v[{}] = {}".format(i, v))
 
+# vw is filled with 1.0 if [w] component is not present in `v` line in .obj
+for i, w in enumerate(attrib.vertex_weights):
+    print("vweight[{}] = {}".format(i, w))
+
 for i, v in enumerate(attrib.normals):
     print("vn[{}] = {}".format(i, v))
 
 for i, v in enumerate(attrib.texcoords):
-    print("vt[{}] = {}".format(i, t))
+    print("vt[{}] = {}".format(i, v))
+
+for i, v in enumerate(attrib.colors):
+    print("vcol[{}] = {}".format(i, v))
+
+if len(attrib.skin_weights):
+    print("num skin weights", len(attrib.skin_weights))
+
+    for i, skin in enumerate(attrib.skin_weights):
+        print("skin_weight[{}]".format(i))
+        print("  vertex_id = ", skin.vertex_id)
+        print("  len(weights) = ", len(skin.weightValues))
+        for k, w in enumerate(skin.weightValues):
+            print("    [{}] joint_id: {}, weight: {}".format(k, w.joint_id, w.weight)) 
 
 if is_numpy_available:
     print("numpy_v = {}".format(attrib.numpy_vertices()))
     print("numpy_vn = {}".format(attrib.numpy_normals()))
     print("numpy_vt = {}".format(attrib.numpy_texcoords()))
+    print("numpy_vcol = {}".format(attrib.numpy_colors()))
 
 materials = reader.GetMaterials()
 print("Num materials: ", len(materials))
@@ -100,6 +127,10 @@ for shape in shapes:
         print("[{}] vn_idx {}".format(i, idx.normal_index))
         print("[{}] vt_idx {}".format(i, idx.texcoord_index))
     print("material_ids = {}".format(shape.mesh.material_ids))
+
+    # faster access to indices
+    a = shape.mesh.vertex_indices()
+    print("vertex_indices", shape.mesh.vertex_indices())
 
     if is_numpy_available:
         print("numpy_indices = {}".format(shape.mesh.numpy_indices()))
